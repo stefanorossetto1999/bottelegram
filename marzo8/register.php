c60eadc43fc3a6089f55e3db997bd76be9384b37
stefanorossetto1999 <45816416+stefanorossetto1999@users.noreply.github.com>
2018-12-12 11:55:37 +0100

    Add files via upload

--- marzo8/register.php
+++ marzo8/register.php
@@ -3,7 +3,7 @@
 		// PARAMETRI DA MODIFICARE
 		//inserite vostro indirizzo su heroku 
 		//https://username.herokuapp.com/execute.php
-		$WEBHOOK_URL = 'https://sandiano-scala-montagne.herokuapp.com/';
+		$WEBHOOK_URL = 'https://sandiano-scala-montagne.herokuapp.com/execute.php';
 		//inserite il vostro token che vi ha fornito l'utente telegram @botfather
 		$BOT_TOKEN = '632416820:AAFDMM0UAWUS5Ge4YXXbXnzaOiRWfXTNwu0' ;
 
