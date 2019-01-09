8ee2e90691d4f5ea218af577489989b88974bf41
stefanorossetto1999 <45816416+stefanorossetto1999@users.noreply.github.com>
2019-01-09 11:56:02 +0100

    Add files via upload

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -47,7 +47,7 @@
 		$parameters = array('chat_id' => $chatId, "text" => $text);
 		if($text=="data"||$text=="/data"){
 			$text="La data odierna è:".date("d.m.y");
-			$parameters= array('chat_id=> $chatId, "text" => $text);
+			$parameters= array('chat_id' => $chatId, "text" => $text);
 			
 			}
 			
